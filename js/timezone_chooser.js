005b475aa29e95913938dbc0971f7bd7f3e0ec5a
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2021-01-04 15:23:40 +0000

    JS: wait for script to finish loading

--- js/timezone_chooser.js
+++ js/timezone_chooser.js
@@ -1,25 +1,31 @@
-let timezone_chooser = document.getElementById('timezone_chooser')
-timezone_chooser.onchange = () => {
-  const userid = timezone_chooser.getAttribute('data-userid')
-  if (!userid) {
+window.setTimeout(() => {
+  let timezone_chooser = document.getElementById('timezone_chooser')
+  if (!timezone_chooser) {
     return
   }
 
-  fetch(Drupal.url('rest/session/token'))
-    .then(data => data.text())
-    .then(token => {
-      fetch(Drupal.url('user/' + userid + '?_format=json'), {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': token
-        },
-        body: JSON.stringify({
-          timezone: [{value: timezone_chooser.value }]
+  timezone_chooser.onchange = () => {
+    const userid = timezone_chooser.getAttribute('data-userid')
+    if (!userid) {
+      return
+    }
+
+    fetch(Drupal.url('rest/session/token'))
+      .then(data => data.text())
+      .then(token => {
+        fetch(Drupal.url('user/' + userid + '?_format=json'), {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': token
+          },
+          body: JSON.stringify({
+            timezone: [{value: timezone_chooser.value }]
+          })
         })
+          .then(data => {
+            location.reload()
+          })
       })
-        .then(data => {
-          location.reload()
-        })
-    })
-}
+  }
+}, 0)
